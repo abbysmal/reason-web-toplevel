6c1c9d8b4c7a3272cdbc8f2d33fb499cce4f88ea
Enguerrand Decorne <decorne.en@gmail.com>
2016-06-18 16:33:11 -0700

    Fix travis (()

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,12 +3,12 @@ sh .travis-ocaml.sh
 
 eval `opam config env`
 
+
+opam install --yes tyxml deriving reactivedata react graphics js_of_ocaml
 opam pin add --yes BetterErrors https://github.com/chenglou/BetterErrors.git
 opam pin add --yes merlin https://github.com/the-lambda-church/merlin.git#87ea0e7998c04f16e4821676c27f19d3879dc2d1
 opam pin add --yes merlin_extend https://github.com/def-lkb/merlin-extend.git#ef634252a793542b05ec00a90f3c17de8fe0a357
 opam pin add --yes higlo https://github.com/Engil/higlo.git#reason-lexer
 opam pin add --yes reason https://github.com/Engil/reason.git#better-findlib
-opam install --yes tyxml deriving reactivedata
-opam install --yes react graphics js_of_ocaml
 cd toplevel
 WITH_GRAPHICS=YES opam config exec -- make
