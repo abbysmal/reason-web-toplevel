e35921bf83d0a0971abe6703fbefe4eb48cf8400
Enguerrand Decorne <decorne.en@gmail.com>
2016-06-18 15:43:50 -0700

    Add opam update

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,6 +7,7 @@ opam pin add --yes merlin https://github.com/the-lambda-church/merlin.git#87ea0e
 opam pin add --yes merlin_extend https://github.com/def-lkb/merlin-extend.git#ef634252a793542b05ec00a90f3c17de8fe0a357
 opam pin add --yes higlo https://github.com/Engil/higlo.git#reason-lexer
 opam pin add --yes reason https://github.com/Engil/reason.git#better-findlib
+opam update --yes
 opam pin add --yes BetterErrors https://github.com/chenglou/BetterErrors.git
 opam install --yes tyxml
 opam install --yes react graphics js_of_ocaml.2.7
