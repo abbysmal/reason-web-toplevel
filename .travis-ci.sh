c55d1a2d77bb2a2f1591edab51d20380ba060948
Enguerrand Decorne <decorne.en@gmail.com>
2016-07-05 17:09:22 -0700

    Fork of reason not needed anymore

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -9,6 +9,6 @@ opam pin add --yes BetterErrors https://github.com/chenglou/BetterErrors.git
 opam pin add --yes merlin https://github.com/the-lambda-church/merlin.git#87ea0e7998c04f16e4821676c27f19d3879dc2d1
 opam pin add --yes merlin_extend https://github.com/def-lkb/merlin-extend.git#ef634252a793542b05ec00a90f3c17de8fe0a357
 opam pin add --yes higlo https://github.com/Engil/higlo.git#reason-lexer
-opam pin add --yes reason https://github.com/Engil/reason.git#better-findlib
+opam pin add --yes reason https://github.com/facebook/reason.git
 cd toplevel
 WITH_GRAPHICS=YES opam config exec -- make
--- README.md
+++ README.md
@@ -6,9 +6,6 @@ To make this word, you need
 - The latest git version of BetterErrors: the one released on opam contains a typo preventing us to build correctly a javascript version of it.
 
 
-- This branch of my fork of Reason: https://github.com/Engil/reason/tree/better-findlib, adding a reason.parser package that does not contains Reason_utop, and also fixing the installation of some modules. (Reason_toolchain for example)
-
-
 - `js_of_ocaml` 2.7, haven't tested 2.8.
 
 
